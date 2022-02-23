7a027260f0c1f315f368e76b168cc50afbf5cc6c
Ignacio Tampe <thenachotp@gmail.com>
2022-02-22 23:00:21 -0800

    Basic Wrapper code and usage of secrets

---
+++ ntfy_wrapper.py
@@ -0,0 +1,28 @@
+from re import A
+import requests
+from requests.auth import HTTPBasicAuth
+
+class NtfyPublisher:
+    def __init__(self, domain, topic, username, password) -> None:
+        self.domain=domain
+        self.topic=topic
+        self.username=username
+        self.password=password
+    
+    def post(self, message, title : str=None, priority=None, tags : list=None):
+        headers = {}
+        
+        if title:
+            headers["Title"] = title
+        if priority:
+            headers["Priority"] = priority
+        if tags:
+            headers["Tags"] = ",".join(tags)
+
+
+        requests.post(
+            f"{self.domain}/{self.topic}",
+            auth= None if not self.username else HTTPBasicAuth(self.username, self.password),
+            data=message,
+            headers=headers
+        )
